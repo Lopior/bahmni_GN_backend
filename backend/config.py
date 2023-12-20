0a70963ebc21e4b958ecffc58f150ae748a750e0
juanjortega <juanjortega@gmail.com>
2023-12-20 20:56:53 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,8 @@
-#Configuracion de la conexion a la BD
+# Configuracion de la conexion a la BD
 
 username = 'root'
 password = 'Lopior.123,'
-host = 'localhost'
+host = 'openmrsdb'  # Replace with the IP address or hostname of your Docker container
 port = 3306
 bd_notificacion = 'bahmni_notification'
 bd_openmrs = 'openmrs'
-
-

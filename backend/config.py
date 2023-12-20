753f2a5ed88347ffc354bf95d42eb3598a908619
juanjortega <juanjortega@gmail.com>
2023-12-20 20:34:06 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 #Configuracion de la conexion a la BD
 
 username = 'root'
-password = 'adminAdmin!123'
+password = 'Lopior.123,'
 host = 'localhost'
 port = 3306
 bd_notificacion = 'bahmni_notification'
